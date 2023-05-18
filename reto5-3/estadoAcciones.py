751b8d68aa5a7e031a3f911e2c5a6d6c3883866a
Tomas Marin A <68928376+tmarina1@users.noreply.github.com>
2023-05-18 17:02:56 -0500

    Add files via upload

---
+++ reto5-3/estadoAcciones.py
@@ -0,0 +1,22 @@
+from mrjob.job import MRJob
+
+class estadoAcciones(MRJob):
+    def mapper(self, _, line):
+        company,price,date = line.split(',')
+        yield company, float(price)
+
+    def reducer(self, company, price):
+        listaPrecio = list(price)
+        precioactual = 0
+
+        for precio in listaPrecio:
+          if precio >= precioactual:
+            estado = 'Se mantine estable'
+            precioactual = precio
+          else:
+            precioactual = precioactual
+            estado = 'No se mantiene estable'
+        yield company, estado
+
+if __name__ == '__main__':
+    estadoAcciones.run()
